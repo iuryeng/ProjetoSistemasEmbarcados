810314c474ccdba14e02f9fa85e4f615003d528e
Iury Coelho <38250160+iuryeng@users.noreply.github.com>
2021-05-29 13:12:47 -0300

    código final em c ++ corrigido

--- pid.ino
+++ pid.ino
@@ -1,22 +1,63 @@
 
+
+class PID{
+public:
+	
+	double error; //erro para realimentacao do sistema
+	double valorLido; //amostra atual
+	double ultimoValorLido; //amostra anterior
+	double kP, kI, kD;       //parametros do sistema de controle com realimentacao
+	double P, I, D;
+	double pid;
+	
+	double setPoint;
+	long lastProcess;
+
+	PID(double _kP, double _kI, double _kD){
+		kP = _kP;
+		kI = _kI;
+		kD = _kD;
+	}
+	
+	void addNewvalorLido(double _valorLido){
+		valorLido = _valorLido;
+	}
+	
+	void setSetPoint(double _setPoint){
+		setPoint = _setPoint;
+	}
+	
+	double process(){
+		// Implementação PID
+		error = setPoint - valorLido;
+		float deltaTime = (millis() - lastProcess) / 1000.0;
+		lastProcess = millis();
+		
+		//P
+		P = error * kP;
+		
+		//I
+		I = I + (error * kI) * deltaTime;
+		
+		//D
+		D = (ultimoValorLido - valorLido) * kD / deltaTime;
+		ultimoValorLido = valorLido;
+		
+		// total
+		pid = P + I + D;
+		
+		return pid;
+	}
+};
+
 #define sensor         A0
 #define controle       3
 
-
-double kP, kI, kD;      
-double P, I, D;
-double pid;
-double error;
-double valorLido;
-double ultimoValorLido;
-	
-double setPoint;
-long lastProcess;	
-
-
+PID PID(1.0, 0, 0);
 
 void setup() {
-	Serial.begin(9600);	
+	Serial.begin(9600);
+	PID.setSetpoint(230.0);
 	pinMode(sensor, INPUT);
 	pinMode(controle, OUTPUT);
 }
@@ -24,23 +65,17 @@ void setup() {
 int controlePwm = 50;
 
 void loop() {
-
-// Implementação PID
-		error = setPoint - valor;
-		float intervaloTempo = (millis() - lastProcess) / 1000.0;
-		lastProcess = millis();		
-		
-		P = error * kP;			
-		I = I + (error * kI) * intervaloTempo;		
-		D = (ultimoValorLido - valor) * kD / intervaloTempo;
-		ultimoValorLido = valor;
-		
-
-		pid = P + I + D;
-
-	double temperature = map(analogRead(sensor), 0, 1023, 0, 100);
 	
-	controlePwm = pid + 50);
+	// Lê temperatura
+	double temperature = map(analogRead(sensor), 0, 1023, 0, 250);
+	
+	// Manda pro objeto PID!
+	PID.addNewvalorLido(temperature);
+	
+	
+	// Converte para controle
+	controlePwm = (PID.process() + 50);
+	// Saída do controle
 	analogWrite(controle, controlePwm);
 	
 }
